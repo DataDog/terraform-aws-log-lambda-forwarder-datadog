183924210ceae706ed5838c880b33fcea7b5e6e6
Vincent Boutour <vincent.boutour@datadoghq.com>
2025-10-16 17:28:45 +0200

    fixup! docs: Align documentation with the one on the forwarder
    Signed-off-by: Vincent Boutour <vincent.boutour@datadoghq.com>

--- main.tf
+++ main.tf
@@ -150,6 +150,8 @@ resource "aws_s3_bucket_policy" "forwarder_bucket_policy" {
 
 # Lambda function
 resource "aws_lambda_function" "forwarder" {
+  region = local.region
+
   function_name = var.function_name
   description   = "Pushes logs, metrics and traces from AWS to Datadog."
   role          = local.iam_role_arn
